4537bd5c11fc85bd6ab1bfa43363d321b8c64869
QuanQinle <quanqinle@gmail.com>
2021-05-11 14:57:43 +0800

    demo for how to open new tab and window

---
+++ src/test/java/com/quanqinle/selenium4trail/Demo01.java
@@ -0,0 +1,36 @@
+package com.quanqinle.selenium4trail;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Open new tab and window
+ *
+ * @author quanql
+ * @version 2021/5/10
+ */
+public class Demo01 {
+    private static final Logger logger = LoggerFactory.getLogger(Demo01.class);
+
+    @BeforeAll
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+    }
+
+    @Test
+    public void testOpenNew() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.navigate().to("https://www.bing.com");
+
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.navigate().to("https://www.bing.com");
+    }
+
+}
